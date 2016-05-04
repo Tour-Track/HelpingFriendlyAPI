6f56fc6ef54ba6fa619a3b10c7d37ce43494965f
Dan Corman <dan.corman@gmail.com>
2016-05-03 20:06:27 -0500

    Added server file

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const koa = require('koa');
+const router = require('koa-router');
+const app = module.exports = koa();
+
+// app.use(router.get('/users', list));
+// app.use(router.get('/users/:id', show));
+
+if (!module.parent) app.listen(3000);
+console.log('running on 3000')
+
+app.use(function * (){
+  this.body = "<body><div style='background-color:black;color:white;text-align:center;padding:5px;' id=''header''><h1>Helping Friendly APP</h1></div><div style='line-height:30px;background-color:#eeeeee;height:300px;width:100px;float:left;padding:5px;'id=''nav''>Phish<br><br></div><div style='width:350px;float:left;padding:10px;' 'id=''section''><h1>Documentation</h1><p>In Progress</p></div><div style='background-color:black;color:white;clear:both;text-align:center;padding:5px; 'id=''footer''></div></body>";
+});
